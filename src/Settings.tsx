44ed155812116a5121ed7284d7efc98c2c02d282
Tharki-God <alokender018@gmail.com>
2023-08-22 07:39:03 +0530

    channel wise toogle

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -7,7 +7,8 @@ export function Settings(): React.ReactElement {
   return (
     <>
       <SwitchItem {...util.useSetting(cfg, "button", true)}>Show Button on ChatBar</SwitchItem>
-      <SwitchItem {...util.useSetting(cfg, "invisible", true)}>Enable Invisible Typing</SwitchItem>
+      <SwitchItem {...util.useSetting(cfg, "channelWise", true)} disabled={!cfg.get("button", true)} key={`${!cfg.get("button", true)}`}>Make Chatbar button toggle it just for channel</SwitchItem>
+      <SwitchItem {...util.useSetting(cfg, "invisible", true)}>Enable Global Invisible Typing</SwitchItem>
     </>
   );
 }
